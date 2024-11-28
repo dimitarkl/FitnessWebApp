055d4f326d388292e95faf4778d0d0cbd65302b0
dimitarkl <158225920+dimitarkl@users.noreply.github.com>
2024-11-28 19:10:25 +0200

    Add Placeholder Navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from './navigation/navigation.component';
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: 'app-root',
+	standalone: true,
+	imports: [RouterOutlet, NavigationComponent],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'FitnessWebApp';
+	title = 'FitnessWebApp';
 }
