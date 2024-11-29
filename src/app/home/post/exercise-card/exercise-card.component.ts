0a00d1782783e159793cd83b311549f536fd3aac
dimitarkl <158225920+dimitarkl@users.noreply.github.com>
2024-11-29 22:53:56 +0200

    Create Exercise Card Placeholder

---
+++ src/app/home/post/exercise-card/exercise-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercise-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './exercise-card.component.html',
+  styleUrl: './exercise-card.component.css'
+})
+export class ExerciseCardComponent {
+
+}
