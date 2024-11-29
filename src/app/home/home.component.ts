fe43160683bc653896deeb35930c48cec2241254
dimitarkl <158225920+dimitarkl@users.noreply.github.com>
2024-11-29 17:45:50 +0200

    Create Placeholder Posts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { PostComponent } from './post/post.component';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+	selector: 'app-home',
+	standalone: true,
+	imports: [PostComponent],
+	templateUrl: './home.component.html',
+	styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
