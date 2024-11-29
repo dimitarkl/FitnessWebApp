d0193a1d16cc71550c67b99c6216c5a37bedbf97
dimitarkl <158225920+dimitarkl@users.noreply.github.com>
2024-11-29 23:45:29 +0200

    Connect Post Components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { PostComponent } from './post/post.component';
+import { CreateWorkoutComponent } from './create-workout/create-workout.component';
 
 @Component({
 	selector: 'app-home',
 	standalone: true,
-	imports: [PostComponent],
+	imports: [PostComponent, CreateWorkoutComponent],
 	templateUrl: './home.component.html',
 	styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+	Card = {
+		username: 'Bradur',
+		workout: 'Full Body Workout',
+	};
+}
--- src/app/home/post/post.component.ts
+++ src/app/home/post/post.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ExerciseCardComponent } from './exercise-card/exercise-card.component';
 
 @Component({
-  selector: 'app-post',
-  standalone: true,
-  imports: [],
-  templateUrl: './post.component.html',
-  styleUrl: './post.component.css'
+	selector: 'app-post',
+	standalone: true,
+	imports: [ExerciseCardComponent],
+	templateUrl: './post.component.html',
+	styleUrl: './post.component.css',
 })
 export class PostComponent {
-
+	@Input() card: {
+		username: string;
+		workout: string;
+	} | null = null;
 }
