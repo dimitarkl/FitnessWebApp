674902238c4b7382efadb4146fb05ec446d235c6
dimitarkl <158225920+dimitarkl@users.noreply.github.com>
2024-11-30 15:40:05 +0200

    Add Placeholder Labels for Sets Inputs

--- src/app/home/create-workout/create-exercise/set-input/set-input.component.ts
+++ src/app/home/create-workout/create-exercise/set-input/set-input.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormGroup, FormsModule, NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-set-input',
-  standalone: true,
-  imports: [],
-  templateUrl: './set-input.component.html',
-  styleUrl: './set-input.component.css'
+	selector: 'app-set-input',
+	standalone: true,
+	imports: [FormsModule],
+	templateUrl: './set-input.component.html',
+	styleUrl: './set-input.component.css',
 })
 export class SetInputComponent {
-
+	@Input() idx: number = 0;
+	@Input() first: boolean = false;
+	submitSet = (form: NgForm) => {
+		// console.log(form);
+		const { weight, reps } = form.value;
+		console.log(`weight:${weight}  and reps${reps}`);
+	};
 }
