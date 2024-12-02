d0157051bfa1d03459eba895210f5af66b7408b7
dimitarkl <158225920+dimitarkl@users.noreply.github.com>
2024-12-02 21:56:21 +0200

    Create Preferences Service

---
+++ src/app/user/preferences.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { auth, db } from '../../lib/firebase';
+import { updateProfile } from 'firebase/auth';
+import {
+	collection,
+	doc,
+	getDocs,
+	query,
+	setDoc,
+	where,
+} from 'firebase/firestore';
+
+@Injectable({
+	providedIn: 'root',
+})
+export class PreferencesService {
+	updateUsername = async (name: string) => {
+		if (auth.currentUser) {
+			try {
+				updateProfile(auth.currentUser, { displayName: name });
+				const body = {
+					userId: auth.currentUser.uid,
+					username: name,
+				};
+				const response = await setDoc(
+					doc(db, 'userPrefs', auth.currentUser.uid),
+					body,
+					{ merge: true }
+				);
+				return response;
+			} catch (e) {
+				console.log('Error Updating User:' + (e as Error));
+			}
+		} else console.log('Error Updating User: User Not Found');
+	};
+	getUsernameById = async (userId: string): Promise<string> => {
+		const q = query(
+			collection(db, 'userPrefs'),
+			where('userId', '==', userId)
+		);
+		const querySnapshot = await getDocs(q);
+		let response = '';
+		querySnapshot.forEach(i => (response = i.data()['username']));
+		return response;
+	};
+}
