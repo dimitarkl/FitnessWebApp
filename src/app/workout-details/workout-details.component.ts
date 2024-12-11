bc11204b1efbfe3c12fc87d948e42c1d664a8c91
dimitarkl <158225920+dimitarkl@users.noreply.github.com>
2024-12-11 14:49:48 +0200

    Update Responsiveness

--- src/app/workout-details/workout-details.component.ts
+++ src/app/workout-details/workout-details.component.ts
@@ -3,6 +3,9 @@ import { WorkoutService } from '../home/workout.service';
 import { ActivatedRoute } from '@angular/router';
 import { Workout } from '../types/Workout';
 import { ExerciseCardComponent } from '../home/post/exercise-card/exercise-card.component';
+import { UserService } from '../user/user.service';
+import { User } from 'firebase/auth';
+import { PreferencesService } from '../user/preferences.service';
 
 @Component({
 	selector: 'app-workout-details',
@@ -14,16 +17,27 @@ import { ExerciseCardComponent } from '../home/post/exercise-card/exercise-card.
 export class WorkoutDetailsComponent implements OnInit {
 	constructor(
 		private workoutService: WorkoutService,
-		private route: ActivatedRoute
+		private route: ActivatedRoute,
+		private userService: UserService,
+		private prefService: PreferencesService
 	) {}
 	workout: Workout | null = null;
+	ownerUsername: string | null = null;
 	ngOnInit(): void {
 		const id = this.route.snapshot.params['detailsId'];
 		this.getWorkout(id);
+		this.getOwnerName();
 	}
+	getOwnerName = () => {
+		if (this.workout?.ownerId)
+			this.prefService
+				.getUsernameById(this.workout.ownerId)
+				.then(response => (this.ownerUsername = response));
+	};
 	getWorkout = (id: string) => {
 		this.workoutService
 			.getWorkoutById(id)
-			.then(response => (this.workout = response));
+			.then(response => (this.workout = response))
+			.then(() => this.getOwnerName());
 	};
 }
