0aacf6b47283587f00c3d44f3f117f6a4a14bbf8
dimitarkl <158225920+dimitarkl@users.noreply.github.com>
2024-12-11 18:59:54 +0200

    Update Home Component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { PostComponent } from './post/post.component';
+//import { PostComponent } from './post/post.component';
 import { WorkoutService } from './workout.service';
 import { Workout } from '../types/Workout';
 import { PreferencesService } from '../user/preferences.service';
@@ -9,23 +9,22 @@ import { ErrorService } from '../error/error.service';
 @Component({
 	selector: 'app-home',
 	standalone: true,
-	imports: [PostComponent, RouterLink],
+	imports: [RouterLink], //add PostComponent
 	templateUrl: './home.component.html',
 	styleUrl: './home.component.css',
 })
 export class HomeComponent implements OnInit {
-	constructor(
-		private workoutService: WorkoutService,
-		private errorService: ErrorService
-	) {}
-	workouts: Workout[] | null = null;
-
+	// 	constructor(
+	// 		private workoutService: WorkoutService,
+	// 		private errorService: ErrorService
+	// 	) {}
+	// 	workouts: Workout[] | null = null;
 	ngOnInit(): void {
-		this.getWorkout();
+		// 		this.getWorkout();
 	}
-	getWorkout = () => {
-		const response = this.workoutService.getLastWorkouts();
-		if (response != null) this.workouts = response;
-		else this.errorService.setError('Error Getting Response');
-	};
+	// 	getWorkout = () => {
+	// 		const response = this.workoutService.getLastWorkouts();
+	// 		if (response != null) this.workouts = response;
+	// 		else this.errorService.setError('Error Getting Response');
+	// 	};
 }
