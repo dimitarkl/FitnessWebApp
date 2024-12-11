947264f8d4b49c7f0b7319f3049caa61121a52fb
dimitarkl <158225920+dimitarkl@users.noreply.github.com>
2024-12-11 18:17:45 +0200

    Create Catalog Component

---
+++ src/app/catalog/catalog.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Workout } from '../types/Workout';
+import { RouterLink } from '@angular/router';
+import { ErrorService } from '../error/error.service';
+import { WorkoutService } from '../home/workout.service';
+import { PostComponent } from '../home/post/post.component';
+@Component({
+	selector: 'app-catalog',
+	standalone: true,
+	imports: [RouterLink, PostComponent],
+	templateUrl: './catalog.component.html',
+	styleUrl: './catalog.component.css',
+})
+export class CatalogComponent {
+	constructor(
+		private workoutService: WorkoutService,
+		private errorService: ErrorService
+	) {}
+	workouts: Workout[] | null = null;
+
+	ngOnInit(): void {
+		this.getWorkout();
+	}
+	getWorkout = () => {
+		const response = this.workoutService.getLastWorkouts();
+		if (response != null) this.workouts = response;
+		else this.errorService.setError('Error Getting Response');
+	};
+}
