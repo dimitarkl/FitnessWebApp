c16134c77be5549111afca6e66a3e37e0f25ecb4
dimitarkl <158225920+dimitarkl@users.noreply.github.com>
2025-03-05 12:51:11 +0200

    Connect To PostgreSQL

--- Server/app.js
+++ Server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool=require('./db')
 app.use(express.json());
 app.use(cors());
 app.listen(7000, (err) => {
@@ -8,4 +9,11 @@ app.listen(7000, (err) => {
 		return console.log("Could not start service");
 	}
 	console.log(`Service started successfully on port 7000`);
+});
+pool.query("SELECT NOW()", (err, res) => {
+  if (err) {
+    console.error("Error executing query", err);
+  } else {
+    console.log("PostgreSQL Time:", res.rows[0]);
+  }
 });
