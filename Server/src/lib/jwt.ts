1c41dcc39f588989417a8eed2b3d48f31580ad0b
dimitarkl <158225920+dimitarkl@users.noreply.github.com>
2025-03-16 13:09:10 +0200

    Install and set up JWT and Bcrypt

---
+++ Server/src/lib/jwt.ts
@@ -0,0 +1,25 @@
+import util from 'util';
+import jwt from 'jsonwebtoken'
+
+const sign = (payload: object, secret: string, options?: jwt.SignOptions): Promise<string> => {
+    return new Promise((resolve, reject) => {
+        jwt.sign(payload, secret, options ?? {}, (err, token) => {
+            if (err) reject(err);
+            else resolve(token as string);
+        });
+    });
+};
+
+const verify = (token: string, secret: string, options?: jwt.VerifyOptions): Promise<object | string> => {
+    return new Promise((resolve, reject) => {
+        jwt.verify(token, secret, options ?? {}, (err, decoded) => {
+            if (err) reject(err);
+            else resolve(decoded as object | string);
+        });
+    });
+};
+
+export default {
+    sign,
+    verify,
+};
